25d0b10c7c40cf7e6d181e7c331ee8a89ed57f6d
svyatoslav-fomin <56718281+svyatoslav-fomin@users.noreply.github.com>
2020-06-29 20:24:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return 'Hello world'
 
+@app.route('/test/<name>')
+def test(name):
+    return 'Hello {0}'.format(name)
+
 if __name__ == '__main__':
     app.run(debug=True)
